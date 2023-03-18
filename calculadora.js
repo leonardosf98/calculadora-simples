5c6a8be637e8dab41fc1376d941ddf582bebe43b
Leonardo <leonardo-f98@hotmail.com>
2023-03-18 13:01:05 -0300

    Resultado funcionando porém com possíveis erros

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,8 @@
+let numero1;
+let numero2;
+let operador;
+
+//Função para teclas numéricas
 let teclas = document.querySelectorAll(".tecla");
 
 teclas.forEach(function (tecla) {
@@ -5,10 +10,55 @@ teclas.forEach(function (tecla) {
     input__calculadora.value += tecla.textContent;
   };
 });
-
+//Função para apagar visor
 let erase = document.querySelector(".erase");
 erase.addEventListener("click", backspace);
 
 function backspace() {
   document.getElementById("input__calculadora").value = "";
+  numero1 = "";
 }
+//Função para operadores matemáticos e armazenamento de valor
+let operadores = document.querySelectorAll(".operadores");
+operadores.forEach(function (sinais) {
+  sinais.onmousedown = function () {
+    numero1 = input__calculadora.value;
+    input__calculadora.value = "";
+    operador = sinais.innerHTML;
+  };
+});
+
+let igual = document.querySelector(".botao__igual");
+
+igual.addEventListener("click", resultado);
+
+function resultado() {
+  if (operador == "/") {
+    numero2 = input__calculadora.value;
+    resultado = numero1 / numero2;
+    input__calculadora.value = resultado;
+    console.log(numero1, numero2);
+  } else if (operador == "*") {
+    numero2 = input__calculadora.value;
+    resultado = numero1 * numero2;
+    input__calculadora.value = resultado;
+    console.log(numero1, numero2);
+  } else if (operador == "-") {
+    numero2 = input__calculadora.value;
+    resultado = numero1 - numero2;
+    input__calculadora.value = resultado;
+    console.log(numero1, numero2);
+  } else if (operador == "+") {
+    numero2 = input__calculadora.value;
+    resultado = numero1 + numero2;
+    input__calculadora.value = resultado;
+    console.log(numero1, numero2);
+  }
+}
+
+/* Colocar números no clique
+Armazenar valores
+aplicar operador matemático
+limpar visor
+
+*/
