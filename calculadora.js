1e47552e881aa5946483880f1a0dbd736214b786
Leonardo <leonardo-f98@hotmail.com>
2023-03-28 18:30:12 -0300

    Calculadora com ajustes no css

--- calculadora.js
+++ calculadora.js
@@ -5,6 +5,14 @@ let operador;
 //Função para teclas numéricas
 let teclas = document.querySelectorAll(".tecla");
 
+const input = document.getElementById("input__calculadora");
+
+input.addEventListener("input", function () {
+  const regex = /^[^\.]*\.[^\.]*$/; // expressão regular que permite apenas um ponto
+  if (!regex.test(this.value)) {
+    this.value = this.value.slice(0, -1); // remove o último caractere (ponto) digitado
+  }
+});
 teclas.forEach(function (tecla) {
   tecla.onmousedown = function () {
     input__calculadora.value += tecla.textContent;
@@ -12,9 +20,9 @@ teclas.forEach(function (tecla) {
 });
 //Função para apagar visor
 let erase = document.querySelector(".erase");
-erase.addEventListener("click", backspace);
+erase.addEventListener("click", deletar);
 
-function backspace() {
+function deletar() {
   document.getElementById("input__calculadora").value = "";
   numero1 = "";
 }
@@ -22,7 +30,7 @@ function backspace() {
 let operadores = document.querySelectorAll(".operadores");
 operadores.forEach(function (sinais) {
   sinais.onmousedown = function () {
-    numero1 = input__calculadora.value;
+    numero1 = input__calculadora.value * 1;
     input__calculadora.value = "";
     operador = sinais.innerHTML;
   };
@@ -34,22 +42,22 @@ igual.addEventListener("click", resultado);
 
 function resultado() {
   if (operador == "/") {
-    numero2 = input__calculadora.value;
+    numero2 = input__calculadora.value * 1;
     resultado = numero1 / numero2;
     input__calculadora.value = resultado;
     console.log(numero1, numero2);
   } else if (operador == "*") {
-    numero2 = input__calculadora.value;
+    numero2 = input__calculadora.value * 1;
     resultado = numero1 * numero2;
     input__calculadora.value = resultado;
     console.log(numero1, numero2);
   } else if (operador == "-") {
-    numero2 = input__calculadora.value;
+    numero2 = input__calculadora.value * 1;
     resultado = numero1 - numero2;
     input__calculadora.value = resultado;
     console.log(numero1, numero2);
   } else if (operador == "+") {
-    numero2 = input__calculadora.value;
+    numero2 = input__calculadora.value * 1;
     resultado = numero1 + numero2;
     input__calculadora.value = resultado;
     console.log(numero1, numero2);
